5f7ee02f76092b20e0d087b1c26b4b4621d002a2
mbellantoni <53529228+mbellantoni@users.noreply.github.com>
2019-12-16 11:47:30 -0500

    Create workout.js

---
+++ workout.js
@@ -0,0 +1,13 @@
+function newLift (int) {
+	if(int == 'low') {
+var percentage = [.60, .625, .65, .675, .7]; }
+	else if (int == 'medium') {
+var percentage = [.725, .75, .775, .8]; }
+	else if (int == 'high') {
+var percentage = [.825, .85, .875, .9]; }
+	else if (int == 'very high') {
+var percentage = [.925, .95, .975]; }
+	else {
+window.alert( "intensity selected doesn't make sense"); }
+return percentage;
+}
